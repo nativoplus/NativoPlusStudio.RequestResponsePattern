5f614247516fa3858fc7bb6ab35b321ade673921
Jose <jcolon@techcu.com>
2020-03-13 12:17:01 -0700

    Adding an error formater

--- NativoPlusStudio.RequestResponsePattern/Error.cs
+++ NativoPlusStudio.RequestResponsePattern/Error.cs
@@ -1,8 +1,20 @@
-﻿namespace NativoPlusStudio.RequestResponsePattern
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Linq;
+namespace NativoPlusStudio.RequestResponsePattern
 {
     public partial class Error 
     {
         public string Code { get; set; }
         public string Message { get; set; }
+
+        public  IList<Error> FormatErrors (ValidationResult validation)
+        {
+            if (!validation.IsValid)
+                return validation?.Errors.Select(x => new Error { Code = x.ErrorCode, Message = x.ErrorMessage }).ToList();
+            else return new List<Error>();
+        
+        }
     }
+
 }
